689bcce29f9601844cee4e415e9b560a7667bcbd
zuren <richardfost@gmail.com>
2014-04-21 02:03:50 +0100

    Created single list
    Wrote a py program to pull all the readme's into one. Did it the long
    way to practice regex

---
+++ getList.py
@@ -0,0 +1,32 @@
+import sys, os, re
+
+def getProjects():
+    categories = {}
+    fullList = []
+    for dirname, dirnames, filenames in os.walk("."):
+        for filename in filenames:
+            checkFilename = re.match("README.md",filename)
+            if checkFilename:
+                f = open(os.path.join(dirname,filename),"rU").read()
+                getCategory = re.match(r"(\w+[\s\w]*)\n----",f)
+                category = getCategory.group(1)
+
+                problems = re.findall(r"\*\*([\w|\s]+)\*\*\s-\s(.+)",f)
+                categories[category] = problems
+    return categories
+
+def createList():
+    categories = getProjects()
+    string = ""
+    for category in categories:
+        string += "\n##"+category+"\n"
+        for project in categories[category]:
+            string += "**"+project[0]+"** - "+project[1]+"\n"
+    f = open("list.md","w")
+    f.write(string)
+
+def main():
+    createList()
+
+if __name__ == "__main__":
+    main()
